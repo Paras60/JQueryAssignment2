a279fdacd2367786697c40f3568e32adaa454fc7
Praveen <praveen.jujare11@gmail.com>
2016-07-27 13:09:35 +0530

    Last Commit

--- public/add.js
+++ public/add.js
@@ -1,37 +1,35 @@
 $(document).ready(function(){
-   $("#add-btn").click(function(){
-       var addItem={
-       "name": $("#inputName").val(),
-       "foot": $("#inputFoot").val(),
-       "age": $("#inputAge").val(),
-       "gender": $("#inputGender").val(), 
-       "sponser": $("#inputSponser").val(),
-	   "email": $("#inputEmail").val(),
-       "phone": $("#inputPhone").val(),
-       "address": $("#inputAddress").val()
-       };
-       
-       $.ajax({
-           type:'POST',
-           url: 'http://localhost:8080/Players',    
-           dataType:'json',
-           data: JSON.stringify(addItem),
-           contentType: "application/json; charset=utf-8", 
-           success: function(data)
-           {   
-                   alert(addItem.name+"'s Record Added Successfully");
-                   $("#myModal").find('form').trigger("reset");
+ $("#add-btn").click(function(){
+   var addItem={
+     "name": $("#inputName").val(),
+     "foot": $("#inputFoot").val(),
+     "age": $("#inputAge").val(),
+     "gender": $("#inputGender").val(), 
+     "sponser": $("#inputSponser").val(),
+     "email": $("#inputEmail").val(),
+     "phone": $("#inputPhone").val(),
+     "address": $("#inputAddress").val()
+   };
 
-               },
-           error: function()
-           {
-                   alert('error Occured');
-           }
-       });
+   $.ajax({
+     type:'POST',
+     url: 'http://localhost:8080/Players',    
+     dataType:'json',
+     data: JSON.stringify(addItem),
+     contentType: "application/json; charset=utf-8", 
+     success: function(data)
+     {   
+       alert(addItem.name+"'s Record Added Successfully");
+     },
+     error: function()
+     {
+       alert('error Occured');
+     }
    });
+ });
 
-   $("#main-btn").click(function(){
-       $("#myModal").find('form').trigger("reset");
-   });
+ $("#main-btn").click(function(){
+   $("#myModal").find('form').trigger("reset");
+ });
 
 });
